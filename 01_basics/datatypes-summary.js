cf382a487763901b1c78acb3ebbb04c34b0d726c
Soumya Majumder <103351485+isoumya16@users.noreply.github.com>
2023-10-08 09:20:12 +0000

    data types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7  types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id);
+// console.log(anotherId);
+
+// console.log(id === anotherId); // return values will not be same however input values are same
+
+// const bigNumber = 3456543576654356754n
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj = {
+    name: "soumya",
+    age : 24
+}
+
+const myFunction = function(){
+console.log("Hello World!");
+}
+
+// console.log(typeof bigNumber);
+
+// console.log(typeof outsideTemp);
+
+// console.log(typeof myFunction);
+
+// console.log(typeof heros);
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions 
+
