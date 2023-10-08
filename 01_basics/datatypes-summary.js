4a964bea996c23b4aba222e8ce6cbb00f89c5ff9
Soumya Majumder <103351485+isoumya16@users.noreply.github.com>
2023-10-08 10:01:15 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -42,3 +42,17 @@ console.log("Hello World!");
 
 // Array, Objects, Functions 
 
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
